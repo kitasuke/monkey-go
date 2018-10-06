26907ab1160329d7a6d26b1ed48cd6f3e7bcf36e
kitasuke <yusuke2759@gmail.com>
2018-10-07 00:52:19 +0900

    Support REPL

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/kitasuke/monkey-go/repl"
 )
 
 func main() {
-	fmt.Printf("Hello")
+	user, err := user.Current()
+	if err != nil {
+		print(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monky programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
