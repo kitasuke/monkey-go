06fbdbaf9a22f74f531747f6b751a5809eeb1788
kitasuke <yusuke2759@gmail.com>
2018-11-03 21:04:38 +0900

    Add simple evaluator

---
+++ evaluator/evaluator.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"github.com/kitasuke/monkey-go/ast"
+	"github.com/kitasuke/monkey-go/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
---
+++ object/object.go
@@ -0,0 +1,35 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	IntegerObj = "INTEGER"
+	BooleanObj = "BOOLEAN"
+	NullObj    = "NULL"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return IntegerObj }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BooleanObj }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NullObj }
+func (n *Null) Inspect() string  { return "null" }
